ae3afda57223e0db5ee1b91bddf5e5a5bf36522d
FuleiNataliya <fulei.nataliya@gmail.com>
2018-09-11 20:23:04 +0300

    change language

--- src/Calculation.java
+++ src/Calculation.java
@@ -3,19 +3,19 @@ public class Calculation {
 
 	public static void main(String[] args) {
 		Trykutnyk t = new Trykutnyk();
-		System.out.printf("Периметр трикутника %s%n", t.perymetrTrykytnyka());
+		System.out.printf("Perimeter of the triangle %s%n", t.perymetrTrykytnyka());
 		Kvadrat k = new Kvadrat();
-		System.out.printf("Периметр квадрата %s%n", k.perymetrKvadrata());
+		System.out.printf("Perimeter of the square %s%n", k.perymetrKvadrata());
 		Pryamokutnyk p = new Pryamokutnyk();
-		System.out.printf("Периметр прямокутника %s%n", p.perymetrPryamokutnyka());
+		System.out.printf("Perimeter of the rectangle %s%n", p.perymetrPryamokutnyka());
 		Paralelogram g = new Paralelogram();
-		System.out.printf("Периметр паралелограма %s%n", g.perymetrPryamokutnyka());
+		System.out.printf("Perimeter of the parallelogram %s%n", g.perymetrPryamokutnyka());
 		Romb r = new Romb();
-		System.out.printf("Периметр ромба %s%n", r.perymetrKvadrata());
+		System.out.printf("Perimeter of diamond %s%n", r.perymetrKvadrata());
 		Kolo o = new Kolo();
-		System.out.printf("Довжина кола %s%n", o.dovgynaKola());
+		System.out.printf("Circle area %s%n", o.dovgynaKola());
 		Chotyrykutnyk ch = new Chotyrykutnyk();
-		System.out.printf("Периметр чотирикутника %s%n", ch.perymetrChotyrukytnyka());
+		System.out.printf("Perimeter of the quadrilateral %s%n", ch.perymetrChotyrukytnyka());
 	}
 
 }
